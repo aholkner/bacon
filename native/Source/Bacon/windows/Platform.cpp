03f9a54ec2ae27f9757105c0c676ebec5ba2a89c
Alex Holkner <alex@spryfox.com>
2013-08-04 22:14:45 +1000

    More stubs to get NativeHost running

---
+++ native/Source/Bacon/windows/Platform.cpp
@@ -0,0 +1,7 @@
+#include "../Bacon.h"
+#include "../BaconInternal.h"
+
+int Bacon_Run()
+{
+    return Bacon_Error_None;
+}
--- native/Source/Bacon/windows/XInputController.cpp
+++ native/Source/Bacon/windows/XInputController.cpp
@@ -11,3 +11,29 @@ void Controller_Shutdown()
 {
 
 }
+
+int Bacon_SetControllerConnectedEventHandler(Bacon_ControllerConnectedEventHandler handler)
+{
+    return Bacon_Error_Unknown;
+}
+
+int Bacon_SetControllerButtonEventHandler(Bacon_ControllerButtonEventHandler handler)
+{
+    return Bacon_Error_Unknown;
+}
+
+int Bacon_SetControllerAxisEventHandler(Bacon_ControllerAxisEventHandler handler)
+{
+    return Bacon_Error_Unknown;
+}
+
+int Bacon_GetControllerPropertyInt(int controller, int property, int* outValue)
+{
+    return Bacon_Error_Unknown;
+}
+
+int Bacon_GetControllerPropertyString(int controller, int property, char* outBuffer, int* inOutBufferSize)
+{
+    return Bacon_Error_Unknown;
+}
+
