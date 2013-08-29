bd936e0b8114bb1380a83cadb0c45d0226097586
Alex Holkner <alex.holkner@gmail.com>
2013-08-29 21:39:34 +1000

    Content scale on glyphs

--- bacon/font.py
+++ bacon/font.py
@@ -89,23 +89,29 @@ class _FontFile(object):
         lib.GetFontMetrics(self._handle, size, byref(ascent), byref(descent))
         return FontMetrics(-ascent.value, -descent.value)
 
-    def get_glyph(self, size, char, flags):
+    def get_glyph(self, size, content_scale, char, flags):
         image_handle = c_int()
         offset_x = c_int()
         offset_y = c_int()
         advance = c_int()
-        lib.GetGlyph(self._handle, size, ord(char), flags,
+        lib.GetGlyph(self._handle, size * content_scale, ord(char), flags,
             byref(image_handle), byref(offset_x), byref(offset_y), byref(advance))
 
         if image_handle.value:
             width = c_int()
             height = c_int()
             lib.GetImageSize(image_handle, byref(width), byref(height))
-            image = bacon.image.Image(width = width.value, height = height.value, handle = image_handle.value)
+            image = bacon.image.Image(width = width.value / content_scale, 
+                                      height = height.value / content_scale, 
+                                      handle = image_handle.value)
         else:
             image = None
 
-        return Glyph(char, image, round(offset_x.value), round(offset_y.value), round(advance.value))
+        return Glyph(char, 
+                     image, 
+                     round(offset_x.value) / content_scale, 
+                     round(offset_y.value) / content_scale, 
+                     round(advance.value) / content_scale)
 
     @classmethod
     def get_font_file(cls, file):
@@ -136,19 +142,27 @@ class Font(object):
 
     Fonts are never unloaded.
 
-    :param file: path to a font file to load.  Supported formats include TrueType, OpenType, PostScript, etc.  If ``None``, a default font is used
-    :param size: the point size to load the font at
-    :param light_hinting: applies minimal autohinting to the outline; suitable for fonts designed 
-       for OS X
+    :param str file: path to a font file to load.  Supported formats include TrueType, OpenType, PostScript, etc.  If ``None``, a default font is used
+    :param float size: the point size to load the font at
+    :param bool light_hinting: applies minimal autohinting to the outline; suitable for fonts designed 
+        for OS X
+    :param float content_scale: optional scaling factor for backing textures of glyphs.  Defaults to
+        to :attr:`Window.content_scale`.
     '''
-    def __init__(self, file, size, light_hinting=False):
+    def __init__(self, file, size, light_hinting=False, content_scale=None):
         if type(file) is _FontFile:
             self._font_file = file
         elif file is None:
             self._font_file = _FontFile.get_default_font_file()
         else:
             self._font_file = _FontFile.get_font_file(file)
+        
         self._size = size
+
+        if content_scale is None:
+            content_scale = bacon.window.content_scale
+        self._content_scale = content_scale
+        
         self._glyphs = { }
         self._flags = 0
 
@@ -183,7 +197,7 @@ class Font(object):
         try:
             return self._glyphs[char]
         except KeyError:
-            glyph = self._font_file.get_glyph(self._size, char, self._flags)
+            glyph = self._font_file.get_glyph(self._size, self._content_scale, char, self._flags)
             self._glyphs[char] = glyph
             return glyph
 
