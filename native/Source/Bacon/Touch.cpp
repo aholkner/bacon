354f7eef068f89dee5fdf8bf6eefde2d34e81ffa
Alex Holkner <alex.holkner@gmail.com>
2013-09-10 14:17:46 +1000

    Tap upper-right and lower-left corners to toggle debug overlay

--- native/Source/Bacon/Touch.cpp
+++ native/Source/Bacon/Touch.cpp
@@ -28,17 +28,44 @@ void Touch_Shutdown()
 {
 }
 
+static void UpdateDebugToggle()
+{
+	bool lowerLeft = false;
+	bool upperRight = false;
+	float radius = 32;
+	int windowWidth, windowHeight;
+	Bacon_GetWindowSize(&windowWidth, &windowHeight);
+	
+	for (int i = 0; i < 2; ++i)
+	{
+		if (!s_Touches[i].m_Pressed)
+			continue;
+		
+		if (s_Touches[i].m_X < radius && s_Touches[i].m_Y > windowHeight - radius)
+			lowerLeft = true;
+		else if (s_Touches[i].m_X > windowWidth - radius && s_Touches[i].m_Y < radius)
+			upperRight = true;
+	}
+	
+	if (lowerLeft && upperRight)
+		DebugOverlay_Toggle();
+}
+
 void Touch_SetTouchState(int touch, int state, float x, float y)
 {
 	if (touch < 0 || touch >= MaxTouches)
 		return;
-	
+
+	bool oldPressed = s_Touches[touch].m_Pressed;
 	s_Touches[touch].m_Pressed = state == Bacon_Touch_State_Pressed ? 1 : 0;
 	s_Touches[touch].m_X = x;
 	s_Touches[touch].m_Y = y;
 	
-	if (s_Handler)
+	if (oldPressed != s_Touches[touch].m_Pressed && s_Handler)
 		(s_Handler)(touch, state, x, y);
+	
+	if (!oldPressed && s_Touches[touch].m_Pressed)
+		UpdateDebugToggle();
 }
 
 int Bacon_SetTouchEventHandler(Bacon_TouchEventHandler handler)
