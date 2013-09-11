f936a1ba019ff12f8725b4b63f103ddb9acd559d
Alex Holkner <alex.holkner@gmail.com>
2013-09-11 21:48:13 +1000

    Enough Python path to load example script and bacon Python module

--- native/Source/PythonHost/PythonHost.cpp
+++ native/Source/PythonHost/PythonHost.cpp
@@ -3,24 +3,36 @@
 
 #include <Python.h>
 
+#include <string>
+using namespace std;
+
+wstring StringToWString(string const& s)
+{
+	std::wstring ws(s.size(), '\0');
+	ws.resize(mbstowcs(&ws[0], s.c_str(), s.size()));
+	return ws;
+}
+
 int main(int argc, char * argv[])
 {
-	// TODO less dodgy
-	wchar_t programName[1024];
-	mbstowcs(programName, argv[0], sizeof(programName));
+	Py_NoSiteFlag = 1;
+	Py_NoUserSiteDirectory = 1;
+	Py_IgnoreEnvironmentFlag = 1;
 	
-	wchar_t pythonHome[1024];
-	mbstowcs(pythonHome, Platform_GetBundlePath(), sizeof(pythonHome));
-	wcscat(pythonHome, L"/Lib");
+	wstring programName = StringToWString(argv[0]);
+	wstring bundlePath = StringToWString(Platform_GetBundlePath());
 	
-    Py_NoUserSiteDirectory++;
-    Py_NoSiteFlag++;
-    Py_IgnoreEnvironmentFlag++;
+	wstring pythonPath = bundlePath + L"/Lib:";
+	pythonPath += L":" + bundlePath;
 	
-	Py_SetProgramName(programName);
-	Py_SetPath(pythonHome);
+	string scriptPath = string(Platform_GetBundlePath()) + "/examples/quickstart.py";
+	
+	FILE* fp = fopen(scriptPath.c_str(), "r");
+	
+	Py_SetProgramName(&programName[0]);
+	Py_SetPath(pythonPath.c_str());
 	Py_InitializeEx(0);
-	PyRun_SimpleString("print('Hello world!')\n");
+	PyRun_SimpleFile(fp, scriptPath.c_str());
 	Py_Finalize();
 	
     return 0;
