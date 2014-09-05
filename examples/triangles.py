b26d53c0e8dc3bcf5c3a399f9f13504586fd38be
Michał Jaworski <swistakm@gmail.com>
2014-09-05 15:41:38 +0200

    examples: update triangles examples

--- examples/triangles.py
+++ examples/triangles.py
@@ -3,9 +3,16 @@ import bacon
 class Game(bacon.Game):
     def on_tick(self):
         bacon.clear(0, 0, 0, 1)
-        bacon.set_color(1, 0, 0, 1)
+
+        bacon.set_color(0, 1, 0, 1)
         bacon.fill_triangle(0, 0, 0, 100, 100, 0)
+        bacon.set_color(1, 0, 0, 1)
+        bacon.draw_triangle(0, 0, 0, 100, 100, 0)
+
         bacon.set_color(0, 1, 0, 1)
         bacon.fill_triangle(100, 100, 150, 100, 200, 300)
-        bacon.draw_rect(400, 400, 500, 500)
+        bacon.set_color(1, 0, 0, 1)
+        bacon.draw_triangle(100, 100, 150, 100, 200, 300)
+
+
 bacon.run(Game())
